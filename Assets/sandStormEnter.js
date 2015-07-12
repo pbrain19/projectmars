7729be91ad9e058415a877a78089a080eb7a36b6
pbrain19 <pbrain19@gmail.com>
2015-07-12 14:43:52 -0400

    added sandStorm

---
+++ Assets/sandStormEnter.js
@@ -0,0 +1,9 @@
+﻿function OnTriggerEnter (other : Collider) {
+	
+	if(other.name == "FPSController") {
+	
+		GameObject.Find('SandStorm').GetComponent.<Animation>().Play('sandStormRise');
+	
+	}
+
+}
