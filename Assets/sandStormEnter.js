b99f1d176b60f7798c44880d84008009686918b6
pbrain19 <pbrain19@gmail.com>
2015-07-12 15:04:04 -0400

    added the motion to the orb during sandstorm... also sandstorm animation

--- Assets/sandStormEnter.js
+++ Assets/sandStormEnter.js
@@ -3,6 +3,7 @@
 	if(other.name == "FPSController") {
 	
 		GameObject.Find('SandStorm').GetComponent.<Animation>().Play('sandStormRise');
+		GameObject.Find('friend').GetComponent.<Animation>().Play('escapeStorm');
 	
 	}
 
