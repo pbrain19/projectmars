fdd7357479ee288eb5fc7a00097cf16f748b417f
Garreth Dottin <gdottin@liveintent.com>
2015-07-12 15:25:00 -0400

    animation for hiding fire

---
+++ Assets/cam.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function OnTriggerEnter (other : Collider) {
+	var battery = GameObject.Find('Battery'); 
+	var fire = GameObject.Find('Fire');
+	Destroy(battery);
+	Destroy(fire);
+}
